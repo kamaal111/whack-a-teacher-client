588cb5c06535a1a8e2dfe8002684e7544d4d0ba0
nicolaurech <urechnicola@gmail.com>
2019-08-12 17:25:38 +0200

    Add basic game interface

--- src/components/GameInterface/index.js
+++ src/components/GameInterface/index.js
@@ -2,11 +2,29 @@ import React from 'react'
 import './GameInterface.css'
 
 export default class GameInterfaceContainer extends React.Component {
+  renderBattlefield = () => {
+    const battlefield = <div id='battlefield'>{generateMole()}</div>
+    return battlefield
+  }
+  
   render() {
     return(
       <div>
-        <div id='battlefield'></div>
+        {this.renderBattlefield()}
       </div>
     )
   }
+}
+
+const generateMole = () => {
+  const randomHeight = Math.max(Math.floor(Math.random() * 550), 0)
+  const randomWidth = Math.max(Math.floor(Math.random() * 550), 0)
+
+  const moleStyle= {
+    top: `${randomHeight}px`,
+    left: `${randomWidth}px`
+  }
+
+  const mole = <div style={moleStyle} className='mole'></div>
+  return mole
 }
