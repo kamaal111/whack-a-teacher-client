affcfb9864628bf71e6ea41f9e7ea294a9b66714
nicolaurech <urechnicola@gmail.com>
2019-08-13 10:51:26 +0200

    Update mole logic

--- src/components/GameInterface/index.js
+++ src/components/GameInterface/index.js
@@ -6,18 +6,52 @@ export default class GameInterfaceContainer extends React.Component {
   state = { 
     moleCount: 0,
     moles: [],
-    score: 0 
+    score: 0,
+    intervalId: 0
   }
 
   componentDidMount () {
-    setInterval(() => {
-      for (let i = 0; i < 1; i++) {
-        this.setState({ 
-          moleCount: this.state.moleCount + 1, 
-          moles: [...this.state.moles, this.renderMole()] 
-        })
-      }
-    }, 1000)
+    // setInterval(() => {
+    //   for (let i = 0; i < 1; i++) {
+    //     this.setState({ 
+    //       moleCount: this.state.moleCount + 1, 
+    //       moles: [...this.state.moles, this.renderMole()] 
+    //     })
+    //   }
+    // }, 1000)
+
+    const intervalId = setInterval(this.launchTimer, 1000)
+    this.setState({ intervalId: intervalId })
+  }
+
+  componentWillUnmount =() => {
+    clearInterval(this.state.intervalId)
+    const moles = document.getElementsByClassName('mole')
+    const molesArray = Array.from(moles)
+    setTimeout(() => {
+      molesArray.forEach(mole => {
+      mole.style.display = 'none'
+    })}, 1000)
+  }
+
+  // generateMoles = () => {
+  //   console.log(this.state.intervalId)
+  //   clearInterval(this.state.intervalId)
+  // }
+
+  launchTimer = () => {
+    for (let i = 0; i < 1; i++) {
+      this.setState({
+        moleCount: this.state.moleCount + 1, 
+        moles: [...this.state.moles, this.renderMole()] 
+      })
+    }
+  }
+
+  whackMole = e => {
+    const mole = document.getElementById(`${e.target.id}`)
+    mole.style.display = 'none'
+    this.setState({ score: this.state.score + 1 })
   }
 
   renderMole = () => {
@@ -31,13 +65,17 @@ export default class GameInterfaceContainer extends React.Component {
 
     const mole = <div 
       style={moleStyle} 
-      className='mole'>
+      className='mole'
+      key={this.state.moleCount}
+      id={`mole-${this.state.moleCount + 1}`}
+      onClick={this.whackMole}>
       </div>
     return mole
   }
   
   render() {
     const moles = this.state.moles
+    console.log(moles)
     if (this.state.moleCount > 3) {
       moles.shift()
     }
@@ -49,6 +87,7 @@ export default class GameInterfaceContainer extends React.Component {
           {moles}
         </div>
         <div className='statistics'><GameStatistics/></div>
+        <button onClick={this.componentWillUnmount}>Stop game</button>
       </div>
     )
   }
